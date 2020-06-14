8088c2abd252660f79e2aeb7b26bd378b781124c
Anand <anandsinghbaghel786@gmail.com>
2020-06-14 11:02:43 +0530

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,6 @@ import { ToastrService } from 'ngx-toastr';
 export class AppComponent implements OnInit {
   title = 'anguar';
   public userLoggedIn: boolean = false;
-
   public userNotAuth: boolean = false;
   public currentUser: Array<User>;
   public token: String = "";
